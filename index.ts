2ae917521ac77ed8c145c0e45ae54908959fb651
damin <34189426+damil27@users.noreply.github.com>
2023-11-05 23:37:24 +0100

    Integration of typeorm

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
-import express,{Express, Request, Response}  from 'express'
-
+import express, { Express, Request, Response } from "express";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+import Cors from "cors"
+import bodyParser from "body-parser";
+import { Task } from "./src/tasks/Task.entity";
 // instanciate the express module
-const app : Express= express();
+const app: Express = express();
+dotenv.config();
+app.use(bodyParser.json())
+app.use(Cors())
 //  define the  port number
-const port = 6200;
+const port = process.env.PORT;
 
+const AppSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities:[Task],
+  synchronize: true,
+});
 
-app.get("/", (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("testing route");
 });
 
-app.listen(port);
+AppSource.initialize()
+  .then(() => {
+    console.log("Conneted to Database Successful!")
+    app.listen(port);
+  })
+  .catch((err) => console.log("error in connecting to database", err));
+
